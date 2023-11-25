f4827f9563113ec1e216a7d1bf52ce9691bb9dbf
Tharun Kumar <tharunstark11@gmail.com>
2023-11-25 18:18:24 +0530

    updated api

--- app/app.py
+++ app/app.py
@@ -2,17 +2,18 @@ from fastapi import FastAPI, status
 from app.scrape import scrape_test, repos_scrape
 app = FastAPI()
 
-@app.get('/', status_code=status.HTTP_403_FORBIDDEN)
+@app.get('/', status_code=status.HTTP_200_OK)
 def root():
-    return {"error": "please enter your username (eg: https://tk-ed.cyclic.app/TK-ed)"}
+    return {"error": "please enter your username (eg: https://tk-ed.cyclic.app/TK-ed) or explore the documentation (https://tk-ed.cyclic.app/docs) for all apis"}
 
 @app.get('/{name}', status_code=status.HTTP_202_ACCEPTED)
 def scrape(name: str):
     url = f'https://www.github.com/{name}'
     return scrape_test(url)
-
-@app.get('/repos/{name}')
+    
+@app.get('/repos/{name}', status_code=status.HTTP_202_ACCEPTED)
 def repo_scrape(name: str):
     url = f"https://www.github.com/{name}?tab=repositories"
     print(url)
     return repos_scrape(url, name)
+    
